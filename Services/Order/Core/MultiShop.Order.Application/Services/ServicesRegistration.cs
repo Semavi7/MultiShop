eeab40a0f9921b937ba754e8535dbc1a9b7514f7
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-04-14 19:25:39 +0300

    MultiShop

---
+++ Services/Order/Core/MultiShop.Order.Application/Services/ServicesRegistration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediatR.Registration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MultiShop.Order.Application.Services
+{
+    public static class ServicesRegistration
+    {
+        public static void AddApplicationService(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(ServicesRegistration).Assembly));
+        }
+    }
+}

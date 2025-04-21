665f58f433443b637eb9a8401c41e5e85fb5a5f5
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-04-21 21:06:36 +0300

    MultiShop

---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultComponents/_CarouselDefaultComponentPertial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultComponents
+{
+    public class _CarouselDefaultComponentPertial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ MultiShop/Frontends/MultiShop.WebUI/ViewComponents/DefaultComponents/_VendorDefaultComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.ViewComponents.DefaultComponents
+{
+    public class _VendorDefaultComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}

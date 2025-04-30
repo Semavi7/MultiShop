b48fe245f03ccc8e119c9632cca8b0d1b929d272
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-04-30 23:01:51 +0300

    MultiShop

--- Frontends/MultiShop.WebUI/Program.cs
+++ Frontends/MultiShop.WebUI/Program.cs
@@ -25,6 +25,10 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{area=Admin}/{controller=Category}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllerRoute(
--- Frontends/MultiShop.WebUI/ViewComponents/DefaultComponents/_CarouselDefaultComponentPertial.cs
+++ Frontends/MultiShop.WebUI/ViewComponents/DefaultComponents/_CarouselDefaultComponentPertial.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.DtoLayer.CatalogDtos.FeatureSliderDtos;
+using Newtonsoft.Json;
 
 namespace MultiShop.WebUI.ViewComponents.DefaultComponents
 {
     public class _CarouselDefaultComponentPertial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _CarouselDefaultComponentPertial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7232/api/FeatureSliders");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsondata = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureSliderDto>>(jsondata);
+                return View(values);
+            }
             return View();
         }
     }

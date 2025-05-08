94e29083c5bf3eef74bca236880091d0dba991c3
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-05-08 12:35:37 +0300

    MultiShop

---
+++ Services/Comment/MultiShop.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.Comment.Context;
+using MultiShop.Comment.Entities;
+
+namespace MultiShop.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _commentContext;
+
+        public CommentsController(CommentContext commentContext)
+        {
+            _commentContext = commentContext;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList()
+        {
+            var values = _commentContext.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetComment(int id)
+        {
+            var values = _commentContext.UserComments.Find(id);
+            return Ok(values);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Add(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarıyla eklendi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Update(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarı ile güncellendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+            var values = _commentContext.UserComments.Find(id);
+            _commentContext.UserComments.Remove(values);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarı ile silindi");
+        }
+
+        [HttpGet("CommentListByProductId")]
+        public IActionResult CommentListByProductId(string id)
+        {
+            var values = _commentContext.UserComments.Where(x => x.ProductId == id).ToList();
+            return Ok(values);
+        }
+
+    }
+}

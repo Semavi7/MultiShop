247ffe63f2a4f4228e8f4e5d8ffee070f2cad67d
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-03 11:58:30 +0300

    MultiShop

---
+++ Services/Message/MultiShop.Message/Program.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using MultiShop.Message.DAL.Context;
+using MultiShop.Message.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<MessageContext>(opt =>
+{
+    opt.UseNpgsql(connectionString);
+});
+
+builder.Services.AddScoped<IUserMessageService, UserMessageService>();
+
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();

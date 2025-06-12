36cd4f98707e8a8b4623066ed323a5a79e90594f
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-12 14:02:09 +0300

    MultiShop

--- Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddCors(opt =>
     });
 });
 
+builder.Services.AddSignalR();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();

f808bc77b019f0acc9c9b51896d738c1429383bc
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-12 12:32:09 +0300

    MultiShop

--- Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
+++ Frontends/MultiShop.WebUI/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeaderComponentPartial.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MultiShop.WebUI.Services.Interface;
+using MultiShop.WebUI.Services.MessageServices;
+using MultiShop.WebUI.Services.StatisticServices.CommentStatisticServices;
 
 namespace MultiShop.WebUI.Areas.Admin.ViewComponents.AdminLayoutViewComponents
 {
     public class _AdminLayoutHeaderComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IMessageService _messageService;
+        private readonly IUserService _userService;
+        private readonly ICommentStatisticService _commentStatisticService;
+
+        public _AdminLayoutHeaderComponentPartial(IMessageService messageService, IUserService userService, ICommentStatisticService commentStatisticService)
         {
+            _messageService = messageService;
+            _userService = userService;
+            _commentStatisticService = commentStatisticService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var user = await _userService.GetUserInfo();
+            int messageCount = await _messageService.GetTotalMessageCountByReceiverId(user.Id);
+            ViewBag.MessageCount = messageCount;
+
+            int totalCommentCount = await _commentStatisticService.GetTotalCommentCount();
+            ViewBag.TotalCommentCount = totalCommentCount;
             return View();
+
         }
     }
 }
---
+++ Services/SignalRRealTime/MultiShop.SignalRRealTimeApi/Services/SignalRCommentServices/SignalRCommentService.cs
@@ -0,0 +1,20 @@
+﻿
+namespace MultiShop.SignalRRealTimeApi.Services.SignalRCommentServices
+{
+    public class SignalRCommentService : ISignalRCommentService
+    {
+        private readonly HttpClient _httpClient;
+
+        public SignalRCommentService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<int> GetTotalCommentCount()
+        {
+            var reponseMessage = await _httpClient.GetAsync("comments/GetTotalCommentCount");
+            var values = await reponseMessage.Content.ReadFromJsonAsync<int>();
+            return values;
+        }
+    }
+}

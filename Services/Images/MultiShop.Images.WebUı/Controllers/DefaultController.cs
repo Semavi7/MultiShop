21cd2f6f7b8a00c287868ca7553494ba0cc3e951
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-14 13:12:51 +0300

    MultiShop

---
+++ Services/Images/MultiShop.Images.WebUı/Controllers/DefaultController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.Images.WebUı.DAL.Entities;
+using MultiShop.Images.WebUı.Services;
+
+namespace MultiShop.Images.WebUı.Controllers
+{
+    public class DefaultController : Controller
+    {
+        private readonly ICloudStorageService _cloudStorageService;
+
+        public DefaultController(ICloudStorageService cloudStorageService)
+        {
+            _cloudStorageService = cloudStorageService;
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ImagesDrive imagesDrive)
+        {
+                if (imagesDrive.Photo != null)
+                {
+                    imagesDrive.SavedFileName = GenerateFileNameToSave(imagesDrive.Photo.FileName);
+                    imagesDrive.SavedUrl = await _cloudStorageService.UploadFileAsync(imagesDrive.Photo,    imagesDrive.SavedFileName);
+                }
+                return RedirectToAction("Index", "Default");
+        }
+
+        private string? GenerateFileNameToSave(string incomingFileName)
+        {
+            var fileName = Path.GetFileNameWithoutExtension(incomingFileName);
+            var extension = Path.GetExtension(incomingFileName);
+            return $"{fileName}-{DateTime.Now.ToUniversalTime().ToString("yyyyMMddHHmmss")}{extension}";
+        }
+    }
+}
+

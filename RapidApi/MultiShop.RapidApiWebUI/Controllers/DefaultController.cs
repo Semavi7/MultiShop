b549e5215a8b58f35bb43a398eadacb2e33ed66b
Mehmet Burçhan Gürses <semavi1151@gmail.com>
2025-06-17 12:48:04 +0300

    MultiShop

---
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/DefaultController.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUI.Models;
+using Newtonsoft.Json;
+
+namespace MultiShop.RapidApiWebUI.Controllers
+{
+    public class DefaultController : Controller
+    {
+        public async Task<IActionResult> WeatherDetail()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://the-weather-api.p.rapidapi.com/api/weather/mumbai"),
+                Headers =
+    {
+        { "x-rapidapi-key", "70ac20abb3msh0370bad0a9cc6fdp126281jsne65064da5db9" },
+        { "x-rapidapi-host", "the-weather-api.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<WeatherViewModel.Rootobject>(body);
+                ViewBag.cityTemp = values.data.temp;
+                return View();
+            }
+        }
+
+        public async Task<IActionResult> Exchange()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=USD&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "70ac20abb3msh0370bad0a9cc6fdp126281jsne65064da5db9" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject> (body);
+                ViewBag.exchange_rateUSD = values.data.exchange_rate;
+                ViewBag.previous_closeUSD = values.data.previous_close;
+            }
+
+            var client2 = new HttpClient();
+            var request2 = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-finance-data.p.rapidapi.com/currency-exchange-rate?from_symbol=EUR&to_symbol=TRY&language=en"),
+                Headers =
+    {
+        { "x-rapidapi-key", "70ac20abb3msh0370bad0a9cc6fdp126281jsne65064da5db9" },
+        { "x-rapidapi-host", "real-time-finance-data.p.rapidapi.com" },
+    },
+            };
+            using (var response2 = await client2.SendAsync(request2))
+            {
+                response2.EnsureSuccessStatusCode();
+                var body = await response2.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ExchangeViewModel.Rootobject>(body);
+                ViewBag.exchange_rateEUR = values.data.exchange_rate;
+                ViewBag.previous_closeEUR = values.data.previous_close;
+                return View();
+            }
+        }
+    }
+}
---
+++ RapidApi/MultiShop.RapidApiWebUI/Controllers/ECommerceController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiShop.RapidApiWebUI.Models;
+using Newtonsoft.Json;
+
+namespace MultiShop.RapidApiWebUI.Controllers
+{
+    public class ECommerceController : Controller
+    {
+        public async Task<IActionResult> ECommerceList()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri("https://real-time-product-search.p.rapidapi.com/search-light-v2?q=Lenova%20Laptop&country=tr&language=tr&page=1&limit=10&sort_by=BEST_MATCH&product_condition=ANY&return_filters=false"),
+                Headers =
+    {
+        { "x-rapidapi-key", "70ac20abb3msh0370bad0a9cc6fdp126281jsne65064da5db9" },
+        { "x-rapidapi-host", "real-time-product-search.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<ECommerceViewModel.Rootobject>(body);
+                return View(values);
+            }
+        }
+    }
+}
